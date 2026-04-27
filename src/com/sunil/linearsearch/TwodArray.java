b00d7f7c837f8cb893715eaa602a498d81e77ac6
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-04-27 23:15:43 +0530

    returning indices and creting an 1d array in return statement

--- src/com/sunil/linearsearch/TwodArray.java
+++ src/com/sunil/linearsearch/TwodArray.java
@@ -1,28 +1,28 @@
 package com.sunil.linearsearch;
-
+import java.util.Arrays;
 public class TwodArray {
     public static void main(String[] args) {
         int[][] arr = {
                 {2, 3, 4, 1},
                 {18, 12, 3, 9},
                 {78, 99, 34, 56},    // target 99 is (2,1)
-                {18, 12},
+                {18, 12}
         };
         int target = 78;
-       int ans = search(arr, target);
-        System.out.println(ans);
+       int [] ans = search(arr, target);
+        System.out.println(Arrays.toString(ans));
 
     }
 
-    static int search(int[][] nums, int target) {
+    static int []  search(int[][] nums, int target) {  //outside loop for row.
         for (int r = 0; r < nums.length; r++) {
-            for (int c = 0; c < nums[r].length; c++) {
+            for (int c = 0; c < nums[r].length; c++) {  //Inner loop is for col.
                 if (nums[r][c] == target) {
-                    return target;
+                    return new int []{r,c};   //we are returning indices in new 1d array.
                 }
             }
         }
-        return -1;
+        return new int  []{-1,-1};
     }
 }
 
